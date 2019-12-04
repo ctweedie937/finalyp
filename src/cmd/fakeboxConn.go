b7dc2104e426cbb5dd4886cfe9eb54d658f825ad
ctweedie937 <claudia@Claudias-MacBook-Pro.local>
2019-12-04 17:19:11 +0000

    Removing node_modules

--- src/cmd/fakeboxConn.go
+++ src/cmd/fakeboxConn.go
@@ -1,29 +1,72 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 const postURL = "http://localhost:8080/fakebox/check/"
 
-func main() {
-
-	resp, err := http.Post(url)
+type ArticleData struct {
+	URL     string `json:"url"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
 }
 
-// gets article and makes request
-func GetArticle(articleURL, title, content string) *http.Response {
-	url := os.Getenv("{}/{}/{}/{}")
-	retrievedURL := fmt.Sprintf(postURL, articleURL, title, content)
+func main() {
+	articleData := ArticleData{
+		URL:     "https://www.bbc.co.uk/news/world-europe-50653597",
+		Title:   "Nato summit: Leaders meet for 70th anniversary amid rows",
+		Content: "Nato leaders are meeting near London...",
+	}
+	jsonStr, err := json.Marshal(&articleData)
 
-	res, err := http.Post(retrievedURL)
+	req, err := http.NewRequest("POST", postURL, bytes.NewBuffer(jsonStr))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
-		println("Unable to fetch request. Error: ", err)
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Status:", resp.Status)
+	fmt.Println("Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("Body:", string(body))
+	getTitle(articleData.URL)
+}
+
+func getTitle(response string) {
+	// converts byte into string
+	articleContent := string(response)
+
+	// searches for title index
+	titleStartIndex := strings.Index(articleContent, "<h1>")
+
+	// if no titles found
+	if titleStartIndex == -1 {
+		fmt.Println("No title element found")
+		os.Exit(0)
 	}
 
-	return res
+	// offset by 7 characters (aka the amount of characters in <title>)
+	titleStartIndex += 7
+
+	// searches for title closing tag
+	titleEndIndex := strings.Index(articleContent, "</h1>")
+	if titleEndIndex == -1 {
+		fmt.Println("No closing tag for title found")
+		os.Exit(0)
+	}
+
+	pageTitle := []byte(articleContent[titleStartIndex:titleEndIndex])
+	fmt.Println("Title: %s", pageTitle)
 }
--- src/ui/views/Analyse/index.jsx
+++ src/ui/views/Analyse/index.jsx
@@ -1,13 +1,37 @@
 import React,  {Component} from "react";
-import { Grid } from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css';
+import { Grid, GridColumn, Header, Segment, Input } from "semantic-ui-react";
 
 const websiteLayout = () => {
-    <Grid celled>
-        <Grid.Row>
-            <Grid.Column width={3}>
-                <h1>"Logo"</h1>
-            </Grid.Column>
-            <Grid.Column width={15}/>
-        </Grid.Row>
-    </Grid>
+    return (
+        <Grid celled divided>
+            <Grid.Row>
+                <Grid.Column width={2}>
+                    <Header size="huge" textAlign="center">Logo</Header>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                    <Header size="huge" textAlign="center">App name</Header>    
+                </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+                <GridColumn width={2}>
+                    <Header size="medium" textAlign="center">Analyse</Header>
+                    <Header size="medium" textAlign="center">Analyse</Header>
+                    <Header size="medium" textAlign="center">Analyse</Header>
+                    <Header size="medium" textAlign="center">Analyse</Header>
+                </GridColumn>
+            </Grid.Row>
+
+            <Grid.Row>
+               <GridColumn width={15}>
+                    <Segment>
+                        <Input action="Analyse" placeholder="Analyse..."/>
+                    </Segment>
+                </GridColumn>
+            </Grid.Row>
+        </Grid>
+    )
 }
+
+export default websiteLayout;
